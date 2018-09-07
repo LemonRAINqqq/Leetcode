6fab74223907f7af1fbe11d46b1525ccc79bd646
LeonCherish <476290209@qq.com>
2018-09-07 15:06:47 +0800

    Create 8_20_isValid.py

---
+++ LeetCode/8_20_isValid.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+20. Valid Parentheses
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+Created on Tue Sep  4 14:44:30 2018
+
+@author: QinLong
+"""
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():#移除列表中的一个元素（默认最后一个元素）
+                    return False
+            else:
+                return False
+        return stack == []
+
+#深刻理解题意后的方法        
+class Solution2(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n = len(s)
+        if n == 0:
+            return True
+        
+        if n % 2 != 0:
+            return False
+            
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('{}','').replace('()','').replace('[]','')
+        
+        if s == '':
+            return True
+        else:
+            return False        
+        
+        
+        
+        
+        
+        
+        
